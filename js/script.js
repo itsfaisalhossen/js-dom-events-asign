d463340a536ee74d084f7de36d9d528f9da8ee42
itsfaisalhossen <itsfaisalhossen@gmail.com>
2025-08-28 00:19:21 +0600

    Heart count functionality feature are added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+// function to get element
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
+}
+// function to get innerText
+function getInnerText(id) {
+  const element = document.getElementById(id);
+  const elementValue = element.innerText;
+  const elementValueNumber = parseInt(elementValue);
+  return elementValueNumber;
+}
+
+// function to set innerText
+function setInnerText(value) {
+  const setElement = getElement("heart-count");
+  setElement.innerText = value;
+}
+
+const allHeartBtn = document.getElementsByClassName("heart-btn");
+for (const heartBtn of allHeartBtn) {
+  heartBtn.addEventListener("click", function (e) {
+    const heartCount = getInnerText("heart-count");
+    const totalCount = heartCount + 1;
+    setInnerText(totalCount);
+  });
+}
